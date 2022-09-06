fd916864eedf520c3850e8c8e94b271a7746fda2
nabilahqurratul <112917436+nabilahqurratul@users.noreply.github.com>
2022-09-05 14:39:43 -1200

    Create soal1.java

---
+++ Praktikum1/soal1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+    public class soal1
+    {
+        public static void main (String[] args)
+        {
+        Scanner beli = new Scanner(System.in);
+        int harga, banyaknya, bayar, diskon, total;
+        System.out.print("Harga Produk per item: ");
+        harga = beli.nextInt();
+
+        System.out.print("Jumlah Pembelian Produk : ");
+        banyaknya = beli.nextInt();
+
+        bayar = harga * banyaknya;
+        diskon = bayar * 15/100;
+        System.out.println("Diskon 15% : " +diskon);
+    
+        total = bayar - diskon;
+        System.out.println("Total Biaya Pembelian Sudah Dipotong Diskonnya Menjadi : " + total);
+    }
+    }
+
+
+
