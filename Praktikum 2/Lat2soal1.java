74d359d43302b4a0a2387016ed81e55564b596ea
nabilahqurratul <112917436+nabilahqurratul@users.noreply.github.com>
2022-09-12 16:37:45 -1200

    Latihan 2 Soal 1 selesai

---
+++ Praktikum 2/Lat2soal1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Lat2soal1
+{
+    public static void main(String args[])
+    {
+        int num1 = 3;
+        int num2 = 2;
+        double Num1 = 3.0;
+        double Num2 = 2;
+        
+        System.out.println("num1 + num2: " + (num1 + num2));
+        System.out.println("num1 - num2: " + (num1 - num2));
+        System.out.println("num1 * num2: " + (num1 * num2));
+        System.out.println("num1 / num2: " + (Num1 / Num2));
+        System.out.println("num1 % num2: " + (num1 % num2));
+    } 
+} 
