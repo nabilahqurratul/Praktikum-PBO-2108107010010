ec1a0a56882ba1f4f343708eb4742cc2ef1382bc
nabilahqurratul <112917436+nabilahqurratul@users.noreply.github.com>
2022-09-12 16:41:03 -1200

    Latihan 2 Soal 2 selesai

---
+++ Lat2soal2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Lat2soal2 {  
+  public static void main(String[] args)
+     {
+        Scanner input=new Scanner(System.in);
+        int marks;
+        System.out.print("Masukkan Nilai: ");
+        marks=input.nextInt();
+
+        if(marks<41){
+            System.out.println("E Grade");
+        }
+        else if(marks>=41 && marks<51){
+            System.out.println("D Grade");
+        }
+        else if(marks>=51 && marks<60){
+            System.out.println("C Grade");
+        }
+        else if(marks>=60 && marks<69){
+            System.out.println("BC Grade");
+        }
+        else if(marks>=69 && marks<78){
+            System.out.println("B Grade"); 
+            
+        }else if(marks>=78 && marks<87){
+            System.out.println("AB grade");
+        }
+        else if(marks>=87 && marks<=100){
+            System.out.println("A Grade");
+        }else{
+            System.out.println("Invalid!");
+        }
+    }
+} 
