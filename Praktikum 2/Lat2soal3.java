54c86cf9545b5b1a8a69208a7aedcddf1d9215b4
nabilahqurratul <112917436+nabilahqurratul@users.noreply.github.com>
2022-09-12 16:55:33 -1200

    Latihan 2 Soal 3 selesai

---
+++ Praktikum 2/Lat2soal3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Lat2soal3
+{
+    public static void main(String arg[])
+    {
+        int a, b;
+        for (a=5; a>=1; a--)
+        {
+            for (b=1; b<=a; b++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
