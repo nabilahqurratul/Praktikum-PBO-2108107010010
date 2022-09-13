f789f1f95b7aaca52f35355c98c1c2f8b8b07648
nabilahqurratul <112917436+nabilahqurratul@users.noreply.github.com>
2022-09-12 16:58:37 -1200

    Latihan 2 Soal 4 selesai

---
+++ Praktikum 2/Lat2soal4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Lat2soal4
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Bilangan Genap Dari 1-20");
+        for (int a=2; a<=18; a+=2)
+        {
+            System.out.print(a+" ");
+        }
+    }
+}
